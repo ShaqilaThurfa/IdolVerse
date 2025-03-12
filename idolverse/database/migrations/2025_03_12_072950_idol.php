841ae3f98f7b5b18b06af371f4b93ef11e24ec16
Shaqila Thurfa <shaqilagunawan20@gmail.com>
2025-03-12 14:51:41 +0700

    feat: user cms and idol table

--- idolverse/database/migrations/2025_03_12_072950_idol.php
+++ idolverse/database/migrations/2025_03_12_072950_idol.php
@@ -11,14 +11,16 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        //
+        Schema::create('idols', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->string('role')->default('admin');
-            $table->rememberToken();
+            $table->string('gender');
+            $table->string('group');
+            $table->string('image');
+            $table->string('birthday');
+            $table->string('position');
+            $table->string('role');
             $table->timestamps();
         });
     }
@@ -28,6 +30,7 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        //
+        Schema::dropIfExists('idols');
     }
 };
