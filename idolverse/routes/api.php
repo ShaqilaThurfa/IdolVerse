95f892f1266dc33b4c1828c4bfbd1c513938e95a
Shaqila Thurfa <shaqilagunawan20@gmail.com>
2025-03-13 12:25:57 +0700

    Mengembalikan perubahan yang hilang dari stash

--- idolverse/routes/api.php
+++ idolverse/routes/api.php
@@ -16,9 +16,9 @@ use App\Http\Controllers\API\UserController;
 |
 */
 
-Route::controller(UserController::class)->group(function () {
-    Route::post('login', 'login');
-    Route::post('register', 'register');
-    Route::post('logout', 'logout');
-    Route::post('refresh', 'refresh');
-});
+// Route::controller(UserController::class)->group(function () {
+//     Route::post('login', 'login');
+//     Route::post('register', 'register');
+//     Route::post('logout', 'logout');
+//     Route::post('refresh', 'refresh');
+// });
