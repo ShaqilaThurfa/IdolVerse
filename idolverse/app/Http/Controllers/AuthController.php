bf272548d08e1852ca3f01412159ca0acc980159
ayuintennurillah <ayuintennurillah08@gmail.com>
2025-03-13 15:14:30 +0700

    Fix: bugu login but without middleware

--- idolverse/app/Http/Controllers/AuthController.php
+++ idolverse/app/Http/Controllers/AuthController.php
@@ -20,20 +20,22 @@ class AuthController extends Controller
     public function login(Request $request)
     {
         $credentials = $request->validate([
-            'email' => 'required|email',
-            'password' => 'required',
+            'email' => ['required', 'email'],
+            'password' => ['required'],
         ]);
 
-
         if (Auth::attempt($credentials)) {
+
             $request->session()->regenerate();
-            return redirect()->route('dashboard');
+
+            return redirect()->intended('/dashboard');
         }
 
         return back()->withErrors([
             'email' => 'Email atau password salah.',
-        ])->withInput();
+        ])->onlyInput('email');
     }
+
     public function register(Request $request)
     {
 
--- idolverse/routes/web.php
+++ idolverse/routes/web.php
@@ -22,13 +22,15 @@ Route::get('/register', function () {
 
 Route::get('/', function () {
     return view('login');
-});
+})->name('login')->middleware('guest');
+
+
 
 Route::post('/register', [AuthController::class, 'register'])->name('register');
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 
 
 // Proses login
-Route::post('/login', [AuthController::class, 'login'])->name('login');
-Route::get('/dashboard', [HomeController::class, 'index'])->name('dashboard')->middleware('auth');
+Route::post('/login', [AuthController::class, 'login'])->name('login.post');
+Route::get('/dashboard', [HomeController::class, 'index'])->name('dashboard');
 
