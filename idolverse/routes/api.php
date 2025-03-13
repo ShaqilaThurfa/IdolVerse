eb7be4a1405a0a7356492b526f97667636b2d2b0
Shaqila Thurfa <shaqilagunawan20@gmail.com>
2025-03-13 09:38:52 +0700

    feat: dashboard argon

--- idolverse/routes/api.php
+++ idolverse/routes/api.php
@@ -1,8 +1,9 @@
 <?php
 
+use App\Http\Controllers\API\UserController as APIUserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\UserController;
+use App\Http\Controllers\API\UserController;
 
 /*
 |--------------------------------------------------------------------------
